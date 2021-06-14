a334024252e5e6c0f18a5cc176c9e316a92b2317
Jason Chandler <Jason.Chandler@leadventure.com>
2021-06-14 11:56:40 -0400

    add perks user selection

--- Responses/User/BaseClasses/FullUser.cs
+++ Responses/User/BaseClasses/FullUser.cs
@@ -171,5 +171,32 @@ namespace TornAPI.Responses.User.BaseClasses
 
         [JsonProperty(PropertyName = "notifications")]
         public Notifications Notifications { get; set; }
+
+        [JsonProperty(PropertyName = "job_perks")]
+        public List<string> JobPerks { get; set; }
+
+        [JsonProperty(PropertyName = "property_perks")]
+        public List<string> PropertyPerks { get; set; }
+
+        [JsonProperty(PropertyName = "stock_perks")]
+        public List<string> StockPerks { get; set; }
+
+        [JsonProperty(PropertyName = "merit_perks")]
+        public List<string> MeritPerks { get; set; }
+
+        [JsonProperty(PropertyName = "education_perks")]
+        public List<string> EducationPerks { get; set; }
+
+        [JsonProperty(PropertyName = "enhancer_perks")]
+        public List<string> EnhancerPerks { get; set; }
+
+        [JsonProperty(PropertyName = "company_perks")]
+        public List<string> CompanyPerks { get; set; }
+
+        [JsonProperty(PropertyName = "faction_perks")]
+        public List<string> FactionPerks { get; set; }
+
+        [JsonProperty(PropertyName = "book_perks")]
+        public List<string> BookPerks { get; set; }
     }
 }
---
+++ Responses/User/Selections/PerksSelection.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace TornAPI.Responses.User.Selections
+{
+    public class PerksSelection : IUserSelection
+    {
+        [JsonProperty(PropertyName = "job_perks")]
+        public List<string> JobPerks { get; set; }
+
+        [JsonProperty(PropertyName = "property_perks")]
+        public List<string> PropertyPerks { get; set; }
+
+        [JsonProperty(PropertyName = "stock_perks")]
+        public List<string> StockPerks { get; set; }
+
+        [JsonProperty(PropertyName = "merit_perks")]
+        public List<string> MeritPerks { get; set; }
+
+        [JsonProperty(PropertyName = "education_perks")]
+        public List<string> EducationPerks { get; set; }
+
+        [JsonProperty(PropertyName = "enhancer_perks")]
+        public List<string> EnhancerPerks { get; set; }
+
+        [JsonProperty(PropertyName = "company_perks")]
+        public List<string> CompanyPerks { get; set; }
+
+        [JsonProperty(PropertyName = "faction_perks")]
+        public List<string> FactionPerks { get; set; }
+
+        [JsonProperty(PropertyName = "book_perks")]
+        public List<string> BookPerks { get; set; }
+
+        public string GetSelectionName()
+        {
+            return SelectionEnums.UserSelections.Perks.ToString();
+        }
+    }
+}
